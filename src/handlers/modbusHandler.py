0e37c31b3470da38fbc51a99ce091c167447b2a5
randelldb <58916132+randelldb@users.noreply.github.com>
2020-12-07 21:38:07 +0100

    installed modbus packages

---
+++ src/handlers/modbusHandler.py
@@ -0,0 +1,19 @@
+import minimalmodbus
+import serial
+
+instrument = minimalmodbus.Instrument('COM6', 1)  # port name, slave address (in decimal)
+
+instrument.serial.port  # this is the serial port name
+instrument.serial.baudrate = 115200  # Baud
+instrument.serial.bytesize = 8
+instrument.serial.parity = serial.PARITY_NONE
+instrument.serial.stopbits = 1
+instrument.serial.timeout = 0.05  # seconds
+
+instrument.address  # this is the slave address number
+instrument.mode = minimalmodbus.MODE_RTU  # rtu or ascii mode
+instrument.clear_buffers_before_each_transaction = True
+
+# Read temperature (PV = ProcessValue) #
+temperature = instrument.read_register(512, 1)  # Registernumber, number of decimals
+print(temperature)
