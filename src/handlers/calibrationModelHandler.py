b8a8f68503a263e5ca436b08c21162fe92df188c
randelldb <58916132+randelldb@users.noreply.github.com>
2020-12-09 08:32:04 +0100

    Added model delete fn

--- src/handlers/calibrationModelHandler.py
+++ src/handlers/calibrationModelHandler.py
@@ -6,6 +6,7 @@ class CalibrationModelHandler:
     @staticmethod
     def create_model(name, model, customer, type_a, a_highValue, a_hvPlus, a_hvMin, a_lowValue, a_lvPlus, a_lvMin,
                      type_b, b_highValue, b_hvPlus, b_hvMin, b_lowValue, b_lvPlus, b_lvMin):
+
         new_model = models.CalibrationModel(name=name, model=model, customer=customer,
                                             type_a=type_a, a_highValue=a_highValue, a_hvPlus=a_hvPlus,
                                             a_hvMin=a_hvMin, a_lowValue=a_lowValue,
@@ -13,13 +14,21 @@ class CalibrationModelHandler:
                                             type_b=type_b, b_highValue=b_highValue, b_hvPlus=b_hvPlus,
                                             b_hvMin=b_hvMin, b_lowValue=b_lowValue,
                                             b_lvPlus=b_lvPlus, b_lvMin=b_lvMin)
-        db.session.add(new_model)
-        db.session.commit()
-        print('Commit!!')
 
+        try:
+            db.session.add(new_model)
+            db.session.commit()
+            print('Creating model success!')
+        except:
+            print('Creating model failed...')
 
-#  name, model, customer,
-# type_a, a_highValue, a_hvPlus, a_hvMin, a_lowValue, a_lvPlus, a_lvMin,
-# type_b, b_highValue, b_hvPlus, b_hvMin, b_lowValue, b_lvPlus, b_lvMin
+    @staticmethod
+    def delete_model(model_id):
+        get_model_id = models.CalibrationModel.query.filter_by(id=model_id).first()
 
-
+        try:
+            db.session.delete(get_model_id)
+            db.session.commit()
+            print('Deleting model success!')
+        except:
+            print('Deleting model failed...')
--- src/routes.py
+++ src/routes.py
@@ -2,10 +2,20 @@ from src import app
 from flask import render_template
 from src.handlers.calibrationModelHandler import CalibrationModelHandler
 
+# CalibrationModelHandler instance
+cm = CalibrationModelHandler()
+
+
 @app.route('/')
 def hello(name=None):
-    # CalibrationModelHandler.create_model('SLT Pass.', 'Emmerson B100', 'NedTrain',
+
+    # Adding model test
+    # cm.create_model('SLT Pass.', 'Emmerson B100', 'NedTrain',
     #              1, 8, 0.5, 0.5, 6, 0.2, 0.2,
     #              2, 2, 0.1, 0.1, 1, 0.1, 0.1)
+
+    # Deleting model test
+    # cm.delete_model(1)
+
 
     return render_template('index.html', name=name)
